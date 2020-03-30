052770bfb13d7a9b52856bb7b5cdc066cc036d6a
Lakshay Mehra <lakshay344@gmail.com>
2020-03-30 14:06:58 +0530

    Updates

---
+++ precache-manifest.c1eb722a3ddd8bc6117c8429aa312ea1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24ccf6e38633084856e395e01006be4c",
+    "url": "/alpha-game/index.html"
+  },
+  {
+    "revision": "fada732627597492968f",
+    "url": "/alpha-game/static/css/2.f524894f.chunk.css"
+  },
+  {
+    "revision": "ed55f7e9558fc5d6639a",
+    "url": "/alpha-game/static/css/main.bd80f7a4.chunk.css"
+  },
+  {
+    "revision": "fada732627597492968f",
+    "url": "/alpha-game/static/js/2.ff0f537e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/alpha-game/static/js/2.ff0f537e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed55f7e9558fc5d6639a",
+    "url": "/alpha-game/static/js/main.52c2892a.chunk.js"
+  },
+  {
+    "revision": "4d64aa3496aaa90e165c",
+    "url": "/alpha-game/static/js/runtime-main.aae916a1.js"
+  }
+]);
